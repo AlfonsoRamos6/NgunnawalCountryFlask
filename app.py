aa7df9827d4ce17a569e9b495e0460cdc664cee7
Alfonso <0257621@schoolsnet.act.edu.au>
2022-07-27 12:51:54 +1000

    template

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.config.from_object(Config)  # loads the configuration for the database
+db = SQLAlchemy(app)            # creates the db object using the configuration
